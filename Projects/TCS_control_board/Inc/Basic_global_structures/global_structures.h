1aa9f74ba798e33041c69ce8883e51a0ffe6c97b
Tytek92 <tymoteusz92@gmail.com>
2016-12-11 22:35:09 +0100

    new files

---
+++ Projects/TCS_control_board/Inc/Basic_global_structures/global_structures.h
@@ -0,0 +1,13 @@
+/*
+ * global_structures.h
+ *
+ *  Created on: 11 gru 2016
+ *      Author: Tymoteusz
+ */
+
+#ifndef BASIC_GLOBAL_STRUCTURES_GLOBAL_STRUCTURES_H_
+#define BASIC_GLOBAL_STRUCTURES_GLOBAL_STRUCTURES_H_
+
+
+
+#endif /* BASIC_GLOBAL_STRUCTURES_GLOBAL_STRUCTURES_H_ */
